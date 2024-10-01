1ad66377275e1c4786188bdec3e79db0200b3b24
Aleksandr Ivanov <haron86@gmail.com>
2024-10-01 02:00:26 -0700

    Refactor booking_id
    fix: remove unnecessary temporary variable booking_id

--- booking_api_test/tests/test_booking.py
+++ booking_api_test/tests/test_booking.py
@@ -16,16 +16,14 @@ class TestBooking:
         logger.info(f"{booking_payload=}")
         booking = booking_api.create_booking(booking_payload)
         logger.info(f"Created booking: {booking}")
-        booking_id = booking.bookingid
-
-        new_booking = booking_api.get_booking(booking_id)
+        new_booking = booking_api.get_booking(booking.bookingid)
         logging.info(f"Checking created booking: {new_booking=}")
         assert new_booking.firstname == booking_payload.firstname
         assert new_booking.lastname == booking_payload.lastname
 
         logger.info("Cleanup: Delete booking")
         booking_api.set_token(token)
-        booking_api.delete_booking(booking_id)
+        booking_api.delete_booking(booking.bookingid)
 
     def test_update_booking(self, booking_api, token):
         booking_api.set_token(token)
@@ -33,20 +31,19 @@ class TestBooking:
         booking_payload = booking_api.payloads.booking()
         booking = booking_api.create_booking(booking_payload)
         logger.info(f"Booking info: {booking}")
-        booking_id = booking.bookingid
-        random_data = booking_api.payloads.booking()
         new_data = booking_payload.model_copy()
+        random_data = booking_api.payloads.booking()
         new_data.firstname = random_data.firstname
         new_data.lastname = random_data.lastname
         logger.info(f"Change first name and last name to: {new_data.firstname}, {new_data.lastname}")
-        response = booking_api.update_booking(booking_id, new_data)
+        response = booking_api.update_booking(booking.bookingid, new_data)
         logger.info("Retrieve updated booking")
-        updated_booking = booking_api.get_booking(booking_id)
+        updated_booking = booking_api.get_booking(booking.bookingid)
         logger.info(f"Updated booking info: {updated_booking=}")
         assert updated_booking.firstname == new_data.firstname
         assert updated_booking.lastname == new_data.lastname
         logger.info("Cleanup: Delete booking")
-        booking_api.delete_booking(booking_id)
+        booking_api.delete_booking(booking.bookingid)
 
     def test_delete_booking(self, booking_api, token):
         booking_api.set_token(token)
@@ -54,10 +51,7 @@ class TestBooking:
         booking_payload = booking_api.payloads.booking()
         booking = booking_api.create_booking(booking_payload)
         logger.info(f"Booking info: {booking}")
-        booking_id = booking.bookingid
-
         logger.info("Delete booking")
-        booking_api.delete_booking(booking_id)
-
+        booking_api.delete_booking(booking.bookingid)
         logger.info("Check if it has been deleted")
-        assert not booking_api.is_booking_exists(booking_id)
+        assert not booking_api.is_booking_exists(booking.bookingid)
