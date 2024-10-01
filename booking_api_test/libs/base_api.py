e49d2dd99f87cae1da6095f9638cf0ace6c4c443
Aleksandr Ivanov <haron86@gmail.com>
2024-10-01 00:27:23 -0700

    Add PATCH method
    fix: added patch method to base_api

--- booking_api_test/libs/base_api.py
+++ booking_api_test/libs/base_api.py
@@ -36,7 +36,7 @@ class BaseAPI:
         response = self.session.get(self.url + "/" + endpoint, params=query)
         ppjson = pprint.pformat(response.json(), indent=2,
                                 compact=True) if response.status_code < 300 else response.content
-        logger.debug(f"GET response: {response.status_code=}, JSON:\n{ppjson}")
+        logger.debug(f"GET result: {response.status_code=}, {expected_status_code=}, JSON:\n{ppjson}")
         if expected_status_code:
             if isinstance(expected_status_code, int):
                 expected_status_code = [expected_status_code]
@@ -55,7 +55,7 @@ class BaseAPI:
         """
         logger.debug(f"POST {self.url=}, {endpoint=}, {data=}")
         response = self.session.post(self.url + "/" + endpoint, json=data)
-        logger.debug(f"POST response: {response.status_code=}")
+        logger.debug(f"POST result: {response.status_code=}, {expected_status_code=}")
         if expected_status_code:
             if isinstance(expected_status_code, int):
                 expected_status_code = [expected_status_code]
@@ -72,7 +72,28 @@ class BaseAPI:
         :param expected_status_code: Optional parameter to check the expected status code
         :return: Return API response as a requests.Response object
         """
+        logger.debug(f"PUT {self.url=}, {endpoint=}, {data=}")
         response = self.session.put(self.url + "/" + endpoint, json=data)
+        logger.debug(f"PUT result: {response.status_code=}, {expected_status_code=}")
+        if expected_status_code:
+            if isinstance(expected_status_code, int):
+                expected_status_code = [expected_status_code]
+            assert response.status_code in expected_status_code, f"Unexpected status code {response.status_code}. Expected status code(s): " + ", ".join(
+                map(str, expected_status_code))
+        return response
+
+    def patch(self, endpoint: str, data: dict,
+              expected_status_code: int | list | None = None) -> requests.Response:
+        """API PATCH Call.
+
+        :param endpoint: API enpoint
+        :param data: Data to change
+        :param expected_status_code: Optional parameter to check the expected status code
+        :return: Return API response as a requests.Response object
+        """
+        logger.debug(f"PATCH {self.url=}, {endpoint=}, {data=}")
+        response = self.session.patch(self.url + "/" + endpoint, json=data)
+        logger.debug(f"PATCH result: {response.status_code=}, {expected_status_code=}")
         if expected_status_code:
             if isinstance(expected_status_code, int):
                 expected_status_code = [expected_status_code]
@@ -88,7 +109,9 @@ class BaseAPI:
         :param expected_status_code: Optional parameter to check the expected status code
         :return: Return API response as a requests.Response object
         """
+        logger.debug(f"DELETE {self.url=}, {endpoint=}")
         response = self.session.delete(self.url + "/" + endpoint)
+        logger.debug(f"DELETE result: {response.status_code=}, {expected_status_code=}")
         if expected_status_code:
             if isinstance(expected_status_code, int):
                 expected_status_code = [expected_status_code]
