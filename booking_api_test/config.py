e7f2d9eddc8f171897b27d6391c0b310563eac2d
Aleksandr Ivanov <haron86@gmail.com>
2024-10-23 01:36:32 -0700

    Delete booking api

--- booking_api_test/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv(".env")
-
-BOOKING_URL = os.environ.get("BOOKING_API_URL", "https://restful-booker.herokuapp.com")
-USERNAME = os.getenv("BOOKING_API_USERNAME")
-PASSWORD = os.getenv("BOOKING_API_PASSWORD")
